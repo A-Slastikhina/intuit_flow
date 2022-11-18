00f120d3b4abcb306cf5f692705644627c90bf3f
A-slastikhina <ao.slastikhina@gmail.com>
2022-11-18 12:22:12 +0600

    11

--- src/js/modules/scroll-header.js
+++ src/js/modules/scroll-header.js
@@ -30,7 +30,7 @@ const scrollHeader = function(){
         
         header.style.backgroundColor='transparent'
         header.style.position = 'absolute';
-        headerIconWA.style.backgroundImage='url("../../img/header/WA.png")'
+        headerIconWA.style.backgroundImage="url('../../img/header/WA.png')"
         headerIconIG.style.backgroundImage='url("../../img/header/Insta.png")'
 
     }
