1c3604952c76f8aca2ce4cc98b02dc17a47b5fd7
A-slastikhina <ao.slastikhina@gmail.com>
2022-11-21 14:29:12 +0600

    11

--- src/js/modules/scroll-header.js
+++ src/js/modules/scroll-header.js
@@ -9,8 +9,8 @@ const headerAdditionText = document.querySelector('.header__addition-text');
  document.addEventListener('scroll', siteScroll)
  function siteScroll (){
     header.style.backgroundColor='hsla(0deg,0%,100%,.9)';
-    header.style.transition='1s'
-    //header.style.position = 'sticky';
+    //header.style.transition='1s'
+ 
     headerIconWA.style.backgroundColor="#A6ACAD";
     headerIconIG.style.backgroundColor="#A6ACAD";
     headerIconIG.style.border = '0.1px solid white';
@@ -29,7 +29,7 @@ const headerAdditionText = document.querySelector('.header__addition-text');
    // header.style.transition = '0.3s'
     if(pageYOffset < 50){
         for (let headerItemLink of headerItemLinks){
-            headerItemLink.style.color = '#edebf6';
+            headerItemLink.style.color = 'white';
             headerItemLink.addEventListener('mouseover', function(){
                 headerItemLink.style.textShadow='none';
             })
@@ -38,14 +38,14 @@ const headerAdditionText = document.querySelector('.header__addition-text');
             })
             //headerItemLink.style.textShadow='none';
         }
-        headerAdditionText.style.color = '#edebf6';
+        headerAdditionText.style.color = 'white';
         header.style.backgroundColor='transparent';
       //  header.style.position = 'absolute';
         headerIconWA.style.backgroundColor="transparent";
         headerIconIG.style.backgroundColor="transparent";
         headerIconIG.style.border = 'none';
         headerIconWA.style.border = 'none';
-       
+      //  header.style.transition='1s'
 
     }
 };
